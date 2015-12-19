477b1593bc911d61dcd8c8aa010f5fb6032f2562
ER Minkush Takkar <erminkushtakkar@ERs-MacBook-Pro.local>
2015-12-20 00:15:26 +0530

    Initial Commit

---
+++ Food Picasa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Picasa
+//
+//  Created by ER Minkush Takkar on 20/12/15.
+//  Copyright (c) 2015 Origa Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Food PicasaTests/Food_PicasaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Food_PicasaTests.swift
+//  Food PicasaTests
+//
+//  Created by ER Minkush Takkar on 20/12/15.
+//  Copyright (c) 2015 Origa Corporation. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Food_PicasaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
